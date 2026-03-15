513bed87d82b5b352eb3a1df057874d6c6a5ffd4
Marcel Otto <marcelotto@gmx.de>
2026-03-15 13:28:42 +0100

    Remove Gno.Application

--- mix.exs
+++ mix.exs
@@ -18,8 +18,7 @@ defmodule Gno.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Gno.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
